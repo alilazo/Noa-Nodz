f690a64293241f7ebeaa612ed11f749938dceb28
Kevin White <58002977+Kevin-White@users.noreply.github.com>
2022-10-21 12:05:43 -0500

    Added Song Page
    There is now a song page for Lil Nas X's Montero, tell me how you like it!

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,73 @@
-console.log("asdasdasd");
+function f1(){
+	var content = document.getElementById("b1").nextElementSibling;
+    if (content.style.display === "block") {
+		content.style.display = "none";
+        document.getElementById("b1").style.width = "24.5%"
+        document.getElementById("b2").style.display = "inline";
+        document.getElementById("b3").style.display = "inline";
+        document.getElementById("b4").style.display = "inline";
+	} else {
+		content.style.display = "block";
+        document.getElementById("b1").style.width = "100%"
+        document.getElementById("b2").style.display = "none";
+        document.getElementById("b3").style.display = "none";
+        document.getElementById("b4").style.display = "none";
+	}
+}
+
+function f2(){
+	var content = document.getElementById("b2").nextElementSibling;
+    if (content.style.display === "block") {
+		content.style.display = "none";
+        document.getElementById("b2").style.width = "24.5%"
+        document.getElementById("b1").style.display = "inline";
+        document.getElementById("b3").style.display = "inline";
+        document.getElementById("b4").style.display = "inline";
+	} else {
+		content.style.display = "block";
+        document.getElementById("b2").style.width = "100%"
+        document.getElementById("b1").style.display = "none";
+        document.getElementById("b3").style.display = "none";
+        document.getElementById("b4").style.display = "none";
+	}
+}
+
+function f3(){
+	var content = document.getElementById("b3").nextElementSibling;
+    if (content.style.display === "block") {
+		content.style.display = "none";
+        document.getElementById("b3").style.width = "24.5%"
+        document.getElementById("b2").style.display = "inline";
+        document.getElementById("b1").style.display = "inline";
+        document.getElementById("b4").style.display = "inline";
+	} else {
+		content.style.display = "block";
+        document.getElementById("b3").style.width = "100%"
+        document.getElementById("b2").style.display = "none";
+        document.getElementById("b1").style.display = "none";
+        document.getElementById("b4").style.display = "none";
+	}
+}
+
+function f4(){
+	var content = document.getElementById("b4").nextElementSibling;
+    if (content.style.display === "block") {
+		content.style.display = "none";
+        document.getElementById("b4").style.width = "24.5%"
+        document.getElementById("b2").style.display = "inline";
+        document.getElementById("b3").style.display = "inline";
+        document.getElementById("b1").style.display = "inline";
+	} else {
+		content.style.display = "block";
+        document.getElementById("b4").style.width = "100%"
+        document.getElementById("b2").style.display = "none";
+        document.getElementById("b3").style.display = "none";
+        document.getElementById("b1").style.display = "none";
+	}
+}
+
+function showVideo(){
+    var video = document.getElementById("cover").nextElementSibling;
+    video.style.display = "block";
+    document.getElementById("cover").style.display = "none";
+}
