d6a682266b11865174a58a970dba28a7147c5e59
Amado Lazo <lazoali98@gmail.com>
2022-10-31 23:35:19 -0500

    Added Dynamic JavaScript Web Pages.
    Added a Song_Template file which hold the DOM manipulated code for Dynamic Web paging using LocalStorage.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,15 +3,15 @@ function f1(){
     if (content.style.display === "block") {
 		content.style.display = "none";
         document.getElementById("b1").style.width = "24.5%"
-        document.getElementById("b2").style.display = "inline";
-        document.getElementById("b3").style.display = "inline";
-        document.getElementById("b4").style.display = "inline";
+        document.getElementById("b2").style.display = "inline"
+        document.getElementById("b3").style.display = "inline"
+        document.getElementById("b4").style.display = "inline"
 	} else {
 		content.style.display = "block";
         document.getElementById("b1").style.width = "100%"
-        document.getElementById("b2").style.display = "none";
-        document.getElementById("b3").style.display = "none";
-        document.getElementById("b4").style.display = "none";
+        document.getElementById("b2").style.display = "none"
+        document.getElementById("b3").style.display = "none"
+        document.getElementById("b4").style.display = "none"
 	}
 }
 
@@ -20,15 +20,15 @@ function f2(){
     if (content.style.display === "block") {
 		content.style.display = "none";
         document.getElementById("b2").style.width = "24.5%"
-        document.getElementById("b1").style.display = "inline";
-        document.getElementById("b3").style.display = "inline";
-        document.getElementById("b4").style.display = "inline";
+        document.getElementById("b1").style.display = "inline"
+        document.getElementById("b3").style.display = "inline"
+        document.getElementById("b4").style.display = "inline"
 	} else {
 		content.style.display = "block";
         document.getElementById("b2").style.width = "100%"
-        document.getElementById("b1").style.display = "none";
-        document.getElementById("b3").style.display = "none";
-        document.getElementById("b4").style.display = "none";
+        document.getElementById("b1").style.display = "none"
+        document.getElementById("b3").style.display = "none"
+        document.getElementById("b4").style.display = "none"
 	}
 }
 
@@ -37,15 +37,15 @@ function f3(){
     if (content.style.display === "block") {
 		content.style.display = "none";
         document.getElementById("b3").style.width = "24.5%"
-        document.getElementById("b2").style.display = "inline";
-        document.getElementById("b1").style.display = "inline";
-        document.getElementById("b4").style.display = "inline";
+        document.getElementById("b2").style.display = "inline"
+        document.getElementById("b1").style.display = "inline"
+        document.getElementById("b4").style.display = "inline"
 	} else {
 		content.style.display = "block";
         document.getElementById("b3").style.width = "100%"
-        document.getElementById("b2").style.display = "none";
-        document.getElementById("b1").style.display = "none";
-        document.getElementById("b4").style.display = "none";
+        document.getElementById("b2").style.display = "none"
+        document.getElementById("b1").style.display = "none"
+        document.getElementById("b4").style.display = "none"
 	}
 }
 
@@ -54,42 +54,63 @@ function f4(){
     if (content.style.display === "block") {
 		content.style.display = "none";
         document.getElementById("b4").style.width = "24.5%"
-        document.getElementById("b2").style.display = "inline";
-        document.getElementById("b3").style.display = "inline";
-        document.getElementById("b1").style.display = "inline";
+        document.getElementById("b2").style.display = "inline"
+        document.getElementById("b3").style.display = "inline"
+        document.getElementById("b1").style.display = "inline"
 	} else {
 		content.style.display = "block";
         document.getElementById("b4").style.width = "100%"
-        document.getElementById("b2").style.display = "none";
-        document.getElementById("b3").style.display = "none";
-        document.getElementById("b1").style.display = "none";
+        document.getElementById("b2").style.display = "none"
+        document.getElementById("b3").style.display = "none"
+        document.getElementById("b1").style.display = "none"
 	}
 }
 
 function showVideo(){
-    var video = document.getElementById("cover").nextElementSibling;
-    video.style.display = "block";
-    document.getElementById("cover").style.display = "none";
+    var video = document.getElementById("cover").nextElementSibling
+    video.style.display = "block"
+    document.getElementById("cover").style.display = "none"
 }
 
 //This is for JavaScript LocalStorage
 
-function As_It_Was_btn(){
-    localStorage.setItem("As_It_Was_btn", "As_It_Was");
-    window.open("Songs/song_template.html", "_self");
+function Listenbtn(localStorageID){
+    localStorage.setItem(localStorageID, localStorageID)
+    window.open("Songs/song_template.html", "_self")
+}
+
+function changeTemplate(pageTitle, songName, artistName, coverPictureSourceURL, spotifySourceURL, appleMusicSourceURL, soundCloudSourceURL, deezerSourceURL, youtubeSourceURL, lyricsSourceURL, songDescription){
+    document.title = pageTitle
+    document.getElementById("Song_Name").innerHTML = songName
+    document.getElementById("Artist_Name").innerHTML = artistName
+    document.getElementById("cover").src = coverPictureSourceURL
+    document.getElementById("spotifySourceURL").src = spotifySourceURL
+    document.getElementById("appleMusicSourceURL").src = appleMusicSourceURL
+    document.getElementById("soundCloudSourceURL").src = soundCloudSourceURL
+    document.getElementById("deezerSourceURL").src = deezerSourceURL
+    
 }
 
 if (document.URL.includes("song_template.html")) {
-    console.log("You are in the song template page");
-    var As_It_Was_btn = localStorage.getItem("As_It_Was_btn");
+    console.log("You are in the song template page")
+    console.log(localStorage.length)
 
-    console.log(As_It_Was_btn);
-
-    if(As_It_Was_btn == "As_It_Was"){
-        document.title = "As It Was";
-        document.getElementById("Song_Name").innerHTML = "As It Was";
-        console.log("Put data here");
-    }
-    //set localStoraged item called As_It_Was_btn to null when one pf the navbar
+        if(localStorage.getItem(localStorage.key(0)).includes("As_It_Was")){
+            changeTemplate(
+            "As It Was", 
+            "As It Was", 
+            "Harry Styles", 
+            "../Artists/HarryStyles/Images/AsItWas.jpg",
+            "https://open.spotify.com/embed/track/4LRPiXqCikLlN15c3yImP7?utm_source=generator&theme=0",
+            "https://embed.music.apple.com/us/album/as-it-was/1615584999?i=1615585008",
+            "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1242349246&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            "https://widget.deezer.com/widget/dark/track/1703487577"
+            
+            )
+        }
+    //set localStoraged item called As_It_Was_btn to null when one pf the navbar and if the back button is pressed
     //localStorage.setItem("As_It_Was_btn", null);
+} else {
+    //TODO: Maybe implement a 404 page not found here
+    localStorage.clear()
 }
