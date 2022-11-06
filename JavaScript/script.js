1b2187cf635a3c85355bf0f4acc8a618137ba39a
Amado Lazo <lazoali98@gmail.com>
2022-11-06 00:41:31 -0500

    Major Change

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -76,7 +76,7 @@ function showVideo(){
 
 function Listenbtn(localStorageID){
     localStorage.setItem(localStorageID, localStorageID)
-    window.open("Songs/song_template.html", "_self")
+    window.open("/Songs/song_template.html", "_self")
 }
 
 function changeTemplate(pageTitle, songName, artistName, coverPictureSourceURL, spotifySourceURL, appleMusicSourceURL, soundCloudSourceURL, deezerSourceURL, youtubeSourceURL, lyricsSourceURL, songDescription){
@@ -97,6 +97,58 @@ function changeTemplate(pageTitle, songName, artistName, coverPictureSourceURL, 
 if (document.URL.includes("song_template.html")) {
     console.log("You are in the song template page")
     console.log(localStorage.length + " " + localStorage.key(0))
+
+    if(localStorage.getItem(localStorage.key(0)).includes("7Ep")){
+        changeTemplate(
+        "Noa-Nodz | 7EP - Lil Nas X", 
+        "7EP", 
+        "Lil Nas X", 
+        "../Albums/Cover Pictures/7EP.jfif",
+        "https://open.spotify.com/embed/album/4IRiXE5NROxknUSAUSjMoO?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/7-ep/1468166325",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/969098314&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        "https://widget.deezer.com/widget/dark/album/100896762"
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("PlanetHer")){
+        changeTemplate(
+        "Noa-Nodz | Planet Her - Doja Cat", 
+        "Planet Her", 
+        "Doja Cat", 
+        "../Albums/Cover Pictures/planet her.jfif",
+        "https://open.spotify.com/embed/album/1nAQbHeOWTfQzbOoFrvndW?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/planet-her-deluxe/1574004234",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1277395135&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://widget.deezer.com/widget/dark/album/238876612"
+        
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("HarrysHouse")){
+        changeTemplate(
+        "Noa-Nodz | Harry's House - Harry Styles", 
+        "Harry's House", 
+        "Harry Styles", 
+        "../Albums/Cover Pictures/harrys house.jfif",
+        "https://open.spotify.com/embed/track/4LRPiXqCikLlN15c3yImP7?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/as-it-was/1615584999?i=1615585008",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1242349246&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://widget.deezer.com/widget/dark/track/1703487577"
+        
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("GooodByeAndGoodRiddance")){
+        changeTemplate(
+        "Noa-Nodz | Good Bye And Good Riddance - Juice Wrld", 
+        "Good Bye And Good Riddance", 
+        "Juice Wrld", 
+        "../Albums/Cover Pictures/Good BYe and Good Riddance.jfif",
+        "https://open.spotify.com/embed/track/4LRPiXqCikLlN15c3yImP7?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/as-it-was/1615584999?i=1615585008",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1242349246&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://widget.deezer.com/widget/dark/track/1703487577"
+        
+        )
+    }
     if(localStorage.getItem(localStorage.key(0)).includes("As_It_Was")){
         changeTemplate(
         "Noa-Nodz | As It Was - Harry Styles", 
@@ -142,10 +194,10 @@ if (document.URL.includes("song_template.html")) {
         "Satisfaction", 
         "Benny Benassi", 
         "../Songs/Cover Pictures/Satisfaction.jfif",
-        "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
-        "https://embed.music.apple.com/us/album/montero/1582660720",
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        "https://widget.deezer.com/widget/dark/album/258772832"
+        "https://open.spotify.com/embed/track/1n7omixiROWs5q6xpWiQuL?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/satisfaction-benny-benassi-presents-the-biz/285865925?i=285865932",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/299148405&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://widget.deezer.com/widget/dark/track/13839055"
         
         )
     }
@@ -154,7 +206,7 @@ if (document.URL.includes("song_template.html")) {
         "Noa-Nodz | Kiss Me More - Doja cat", 
         "Kiss Me More", 
         "Doja Cat", 
-        "../Songs/Cover Pictures/KissMeMore.jfif",
+        "../Songs/Cover Pictures/KissMeMore-DojaCat.jpg",
         "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
         "https://embed.music.apple.com/us/album/montero/1582660720",
         "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
@@ -164,10 +216,10 @@ if (document.URL.includes("song_template.html")) {
     }
     if(localStorage.getItem(localStorage.key(0)).includes("ILikeYou")){
         changeTemplate(
-        "Noa-Nodz | I Like You - Doja Cat", 
-        "Montero", 
-        "Lil Nas X", 
-        "../Artists/LilNasX/Images/MontaroAlbum.jfif",
+        "Noa-Nodz | I Like You - Post Malone, Doja Cat", 
+        "I Like You", 
+        "Doja Cat", 
+        "../Songs/Cover Pictures/ILikeyou-DojaCat.jpg",
         "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
         "https://embed.music.apple.com/us/album/montero/1582660720",
         "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
@@ -177,10 +229,36 @@ if (document.URL.includes("song_template.html")) {
     }
     if(localStorage.getItem(localStorage.key(0)).includes("IAmAGummyBear")){
         changeTemplate(
-        "Noa-Nodz | I Am A Gummy Bear", 
-        "Montero", 
-        "Lil Nas X", 
-        "../Artists/LilNasX/Images/MontaroAlbum.jfif",
+        "Noa-Nodz | I am a Gummy Bear", 
+        "I am a Gummy Bear", 
+        "The Gummy Bear Song", 
+        "../Songs/Cover Pictures/IAmAGummyBear-GummyBear.jpg",
+        "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/montero/1582660720",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        "https://widget.deezer.com/widget/dark/album/258772832"
+        
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("Poland")){
+        changeTemplate(
+        "Noa-Nodz | Poland - Lil Yachty", 
+        "Poland", 
+        "Lil Yachty", 
+        "../Songs/Cover Pictures/Poland-LilYachty.jpg",
+        "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/montero/1582660720",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        "https://widget.deezer.com/widget/dark/album/258772832"
+        
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("Grove")){
+        changeTemplate(
+        "Noa-Nodz | Grove St. Party - Waka Flocka Flame", 
+        "Groove St. Party", 
+        "Waka Flocka Flame", 
+        "../Songs/Cover Pictures/GroveStParty-WakaFlockaFlame.jpg",
         "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
         "https://embed.music.apple.com/us/album/montero/1582660720",
         "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
@@ -193,11 +271,24 @@ if (document.URL.includes("song_template.html")) {
         "Noa-Nodz | Get It To It - Doja Cat", 
         "Get It To It", 
         "Doja Cat", 
-        "../Songs/Cover Pictures/GetItToIt.jpeg",
+        "../Songs/Cover Pictures/GetIntoIt-DojaCat.jpg",
         "https://open.spotify.com/embed/album/6pOiDiuDQqrmo5DbG0ZubR?utm_source=generator&theme=0",
         "https://embed.music.apple.com/us/album/montero/1582660720",
         "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1316422069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
         "https://widget.deezer.com/widget/dark/album/258772832"
+        
+        )
+    }
+    if(localStorage.getItem(localStorage.key(0)).includes("Shadowbringers")){
+        changeTemplate(
+        "Noa-Nodz | Shadowbringers - Final Fantasy XIV: Shadowbringers", 
+        "Shadowbringers", 
+        "Final Fantasy XIV: Shadowbringers", 
+        "../Songs/Cover Pictures/Shadowbringers-Soken.jpg",
+        "https://open.spotify.com/embed/track/1MOsWaaMx4L7YUYGu7LdEx?utm_source=generator&theme=0",
+        "https://embed.music.apple.com/us/album/shadowbringers/1477136155?i=1477136551",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/657272936&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://widget.deezer.com/widget/dark/track/731353052"
         
         )
     }
